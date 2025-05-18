b5c1faa9008d94d79569a3a82bcfce108fbc5fba
Riya Garjola <163320653+riyagarjola@users.noreply.github.com>
2025-05-18 07:27:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,79 @@
-//console alert prompt
-/*console.log("Riya Garjola")
-console.warn("Riya Garjola")
-console.error("Riya Garjola")*/
+//Functions
+//es5 and es6
+/*es5->1.function statements
+       2.function expressions
+       3.anonymous function
+*/
+/*es6->fat arrow function->1.basic fat arrow
+                           2.fat arrow with one param 
+                           3.fat arrow with implicit return*/
 
-/*alert("Devil is warching you");
-//prompt alert se pehle aata h aur voh kuch question poochega vaha pr */
-//var nam = prompt("What is your Name?");
+/*functions - js m functions normal programming language jese nhi hote h , yaha pr js m function bnane k liye type nhi bnana padta, bs funvtion 
+keyword likhna padega and give it a name also*/
+/*
+function abcd(~param){
+}
+*/
+/*js m functions ko first class functions ka darja diya gaya h, jiska mtlv h ki js m functions ko value/variables ki tarah treat kiya jata h */
+/*
+function abcd(){
+var i=12;
+console.log(i)
+for(var b=12;b<22;b++){
+console.log("hello")
+}
+ }
 
-/*var discount = 10;
-discount = 22;
+abcd(); */ 
+/*function abcd(val){
+    console.log(12+val)
+}
+abcd(12);*/
 
-const discount =12;
-discount = 23;  */
+/*function abcd(){
+   //Function Statement
+}*/ 
 
-/* Datatypes---> harsh-->string
-13-->integer
-10.0-->float
-true-->boolean
-a-->character
-"323iufoig"-->string
- */
+/*var abcd=function(){
+   //Function Expression
+}*/
 
-/*types--> primitive dt || refererence dt
+/*function(){
+    //Anonymous Function
+}*/
 
-[] () {}  ----->reference 
-     other primitive like 12, null, a true undefined*/
-     
-/*var = 12;
-var b = a * / 
+//Fat Arrow
+/*var a = ()=>{};
+var b=()=>{
+    
+}
+var c=()=>{
+    
+}*/
+//Fat arrow with one parameter
+/*var g = (ab)=>{}
+    
+g(12)*/ 
 
-/* var arr = [1,2,3,4,5,6];
-var b = arr;
-b.pop();*/
+//Implicit Return 
+/*var abcd = ()=>"harsh"
+abcd()*/
 
-/*  */ 
+//return ka matlab hua,jaha par bhi return lgega uske aage jo bhi likhoge wo jaayega jaha pr function call hua tha,return function k under lgta h
+/*function abcd(){
+    return 12;
+}
+console.log(abcd());*/
+
+//if return no value then --> undefined
+//asa function jo kuch return nhi krta vo bhi undefined return krta h.
+
+//Fat arrow Example
+/*var a = ()=>{
+    return 12;
+}
+var b=a();*/
+
+//Implicit Return
+var a=()=>12;
+var b=a();
