7119435dcc9cb297c464dfb04381c3fb6a871dc3
Riya Garjola <163320653+riyagarjola@users.noreply.github.com>
2025-05-18 07:26:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//console alert prompt
+/*console.log("Riya Garjola")
+console.warn("Riya Garjola")
+console.error("Riya Garjola")*/
+
+/*alert("Devil is warching you");
+//prompt alert se pehle aata h aur voh kuch question poochega vaha pr */
+//var nam = prompt("What is your Name?");
+
+/*var discount = 10;
+discount = 22;
+
+const discount =12;
+discount = 23;  */
+
+/* Datatypes---> harsh-->string
+13-->integer
+10.0-->float
+true-->boolean
+a-->character
+"323iufoig"-->string
+ */
+
+/*types--> primitive dt || refererence dt
+
+[] () {}  ----->reference 
+     other primitive like 12, null, a true undefined*/
+     
+/*var = 12;
+var b = a * / 
+
+/* var arr = [1,2,3,4,5,6];
+var b = arr;
+b.pop();*/
+
+/*  */ 
